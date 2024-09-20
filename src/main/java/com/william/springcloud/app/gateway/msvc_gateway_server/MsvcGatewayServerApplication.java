5ad9e105fab4a136bc6669fa1c4225ddec4c7953
William Wun <williamwun@hotmail.com>
2024-09-20 12:37:36 -0700

    s4 project started

---
+++ src/main/java/com/william/springcloud/app/gateway/msvc_gateway_server/MsvcGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.william.springcloud.app.gateway.msvc_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/springcloud/app/gateway/msvc_gateway_server/MsvcGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.springcloud.app.gateway.msvc_gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
